69668f5b8423794ca69101a2b358b1e660020f0b
Alon <2315079583@qq.com>
2024-08-19 14:54:09 +0800

    桌面ui

--- javaSrc/src/ice/Alon/content/items/KtItems.kt
+++ javaSrc/src/ice/Alon/content/items/KtItems.kt
@@ -9,19 +9,15 @@ class KtItems : Any() {
         lateinit var lonelyHeatSoreSpore: Item
         fun load() {
             /** 晶状孢芽 */
-            crystallineSpore = object : Item("crystallineSpore") {
-                init {
-                    flammability = 0.2f
-                }
+            crystallineSpore = object : Item("crystallineSpore") {}.apply {
+                flammability = 0.2f
             }
             /** 灼热孢团 */
-            pyroSpore = object : Item("pyroSpore") {
-                init {
-                    explosiveness = 0.1f
-                }
+            pyroSpore = object : Item("pyroSpore") {}.apply {
+                explosiveness = 0.1f
             }
             /** 寂温疮体 */
-            lonelyHeatSoreSpore = object : Item("lonelyHeatSoreSpore") {}
+            lonelyHeatSoreSpore = object : Item("lonelyHeatSoreSpore") {}.apply { }
         }
     }
 }
--- javaSrc/src/ice/Alon/ui/MenusDialog.kt
+++ javaSrc/src/ice/Alon/ui/MenusDialog.kt
@@ -6,7 +6,6 @@ import arc.graphics.Color
 import arc.input.KeyCode
 import arc.scene.event.InputEvent
 import arc.scene.event.InputListener
-import arc.scene.ui.Button
 import arc.scene.ui.TextButton.TextButtonStyle
 import arc.scene.ui.layout.Table
 import ice.Alon.asundry.BaseTool.ToolUi
@@ -17,97 +16,113 @@ import mindustry.gen.Sounds
 import mindustry.ui.Fonts
 import java.util.*
 
+val deepSpace = IceDialog()
 
 fun init() {
-    val f: TextButtonStyle = object : TextButtonStyle() {
-        init {
-            over = ToolUi.createFlatDown("ice-flat-down-base")
-            up = ToolUi.createFlatDown("ice-flat-down-base1")
-            down = up
-            font = Fonts.def
-            fontColor = Color.valueOf("97abb7")
-            disabledFontColor = Color.gray
+    val sound = object : InputListener() {
+        val l = Sounds.breaks
+        override fun touchDown(event: InputEvent?, x: Float, y: Float, pointer: Int, button: KeyCode?): Boolean {
+            l.play()
+            return super.touchDown(event, x, y, pointer, button)
         }
     }
+    val button = object : TextButtonStyle() {}.apply {
+        over = ToolUi.createFlatDown("ice-do")
+        up = ToolUi.createFlatDown("ice-de")
+        down = up
+        font = Fonts.def
+        fontColor = Color.valueOf("97abb7")
+        disabledFontColor = Color.gray
+    }
+    val f = object : TextButtonStyle() {}.apply {
+        over = ToolUi.createFlatDown("ice-flat-down-base")
+        up = ToolUi.createFlatDown("ice-flat-down-base1")
+        down = up
+        font = Fonts.def
+        fontColor = Color.valueOf("97abb7")
+        disabledFontColor = Color.gray
+    }
     val createFlatDown = ToolUi.createFlatDown("ice-1")
-    val h = IceDialog()
-    h.cont.setBackground(createFlatDown)
-    h.cont.margin(10f, 10f, 10f, 10f)
-    h.getCell(h.cont).size((Core.graphics.width - 20).toFloat(), (Core.graphics.height - 10).toFloat())
-    h.cont.table { t: Table ->
-
-        t.table { p: Table ->
+    fun k(table: Table) {
+        table.background = createFlatDown
+    }
+    deepSpace.cont.setBackground(createFlatDown)
+    deepSpace.cont.margin(10f, 10f, 10f, 10f)
+    deepSpace.getCell(deepSpace.cont).size((Core.graphics.width - 20).toFloat(), (Core.graphics.height - 10).toFloat())
+    deepSpace.cont.table { t ->
+        t.addListener(sound)
+        t.table { p ->
             p.image(IceItems.leadIngot.fullIcon).left()
-            p.button({ b: Button ->
+            p.button({ b ->
                 b.setStyle(f)
                 b.image(Icon.copy).left()
                 b.add("@close")
-            }) { h.hide() }
-            p.background = createFlatDown
-        }.pad(2f).fill().expand().row()
-        t.table { p: Table ->
-            p.image(IceItems.copperIngot.fullIcon).left()
-            p.button({ b: Button ->
-                b.setStyle(f)
-                b.image(Icon.copy).left()
-                b.add("@close")
-            }) { h.hide() }
-            p.background = createFlatDown
+            }) { deepSpace.hide() }
+            k(p)
         }.pad(2f).fill().expand().row()
 
-        t.table { p: Table ->
-            p.image(IceItems.sphalerite.fullIcon).left()
-            p.button({ b: Button ->
+        t.table { p ->
+            p.image(IceItems.copperIngot.fullIcon).left()
+            p.button({ b ->
                 b.setStyle(f)
                 b.image(Icon.copy).left()
                 b.add("@close")
-            }) { h.hide() }
-            p.background = createFlatDown
+            }) { deepSpace.hide() }
+            k(p)
         }.pad(2f).fill().expand().row()
+
+        t.table { p ->
+            p.margin(10f)
+            p.button({ b ->
+                b.setStyle(f)
+                b.image(Icon.settings).left()
+                b.add("@settings")
+            }) { deepSpace.hide() }.fill().expand()
+            k(p)
+        }.pad(2f).fill().expand().row()
+
+        t.button({ b ->
+            b.setStyle(button)
+            b.image(Icon.settings).left()
+            b.add("@settings")
+        }) {}.pad(2f).fill().expand().row()
+
+        t.button({ b ->
+            b.setStyle(button)
+            b.image(Icon.bookOpen).left()
+            b.add("@ExpressOne.sThanks")
+        }) { }.pad(2f).fill().expand().row()
+
+        t.button({ b ->
+            b.setStyle(button)
+            b.image(Icon.exit).left()
+            b.add("@close")
+        }) { deepSpace.hide() }.pad(2f).fill().expand().row()
+
+
     }.left().width(200f).fillY().expandY()
 
-    h.cont.table { t: Table ->
+    deepSpace.cont.table { t ->
         t.add(
             FLabel(
                 """
-                    ddfeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
-                    eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
-                    eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
-                    eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
-                    eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
-                    eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
-                    eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
-                    eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
-                    eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
-                    eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
-                    eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
-                    eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
-                    eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
-                    eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
-                    eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+                    eeeeeeeeeeeeeeeeeeeeeeeee
                     """.trimIndent()
             )
-        )
-        t.button("1234") {}.table.addListener(object : InputListener() {
-            val l = Sounds.breaks
-            override fun touchDown(
-                event: InputEvent, x: Float, y: Float, pointer: Int, button: KeyCode
-            ): Boolean {
-                l.play()
-                return super.touchDown(event, x, y, pointer, button)
-            }
-
-            override fun touchUp(
-                event: InputEvent, x: Float, y: Float, pointer: Int, button: KeyCode
-            ) {
-                l.stop()
-                super.touchUp(event, x, y, pointer, button)
-            }
-        })
+        ).row()
+        t.button("1234") {}.table.addListener(sound)
     }.fill().expand()
+
+    deepSpace.cont.table { t ->
+        t.table { p ->
+            p.image(Core.atlas.find("ice-hua"))
+        }.expandY().bottom()
+    }.width(333f).right().fillY()
+
+
     val color = arrayOf("#C384B7", "red")
     Vars.ui.menufrag.addButton(
         "[" + color[Random().nextInt(color.size)] + "]" + "DeepSpace[]", Icon.book
-    ) { h.show() }
+    ) { deepSpace.show() }
 
 }
