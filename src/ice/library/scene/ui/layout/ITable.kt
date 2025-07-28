e56c813d17b6bd37359516a1f70fa4ad15533662
Alone <2894761300@qq.com>
2025-07-28 19:33:13 +0800

    text

---
+++ src/ice/library/scene/ui/layout/ITable.kt
@@ -0,0 +1,21 @@
+package ice.library.scene.ui.layout
+
+import arc.scene.Element
+import arc.scene.style.Drawable
+import arc.scene.ui.layout.Cell
+import arc.scene.ui.layout.Table
+
+class ITable(back: Drawable? = null) : Table(back) {
+    private var rowsiz = 0
+    fun setRowsize(int: Int) {
+        rowsiz = int
+    }
+
+    override fun <T : Element> add(element: T): Cell<T> {
+        val size = cells.size
+        if (rowsiz != 0 && size % rowsiz == 0) {
+            row()
+        }
+        return super.add(element)
+    }
+}
