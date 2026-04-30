0a5d910da996eb64829c8df352c789adac6bcd56
Alone <2894761300@qq.com>
2026-05-01 07:05:46 +0800

    更新

--- src/ice/content/block/liquid/涡流泵.kt
+++ src/ice/content/block/liquid/涡流泵.kt
@@ -8,6 +8,7 @@ import mindustry.world.blocks.production.Pump
 import mindustry.world.draw.DrawDefault
 import mindustry.world.draw.DrawMulti
 import mindustry.world.draw.DrawPumpLiquid
+import mindustry.world.draw.DrawRegion
 
 class 涡流泵:Pump("vortexPump") {
   init  {
@@ -21,7 +22,7 @@ class 涡流泵:Pump("vortexPump") {
     squareSprite = false
     pumpAmount = 0.5f
     liquidCapacity = 180f
-    drawer = DrawMulti(DrawPumpLiquid(),DrawDefault())
+    drawer = DrawMulti(DrawRegion("-bottom"), DrawPumpLiquid(), DrawDefault())
     requirements(Category.liquid, IItems.铬锭, 50, IItems.锌锭, 40, IItems.黄铜锭, 25, IItems.石英玻璃, 30, IItems.钴锭, 30)
   }
 }
